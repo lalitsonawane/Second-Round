fb920d53f4263bbd336de2aa92b71c9d5f27c31c
Lalit Sonawane <appdeveloper.lalit@gmail.com>
2018-06-15 12:42:54 +0530

    Initial commit

---
+++ app/src/main/java/in/apptonic/secondround/MainActivity.java
@@ -0,0 +1,29 @@
+package in.apptonic.secondround;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txt;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txt = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt.setText("lalit ka application....");
+            }
+        });
+    }
+}
